7fd0daadc5a48010ff3f0d28183ea80429120723
riooastfu <rio.alaska123@gmail.com>
2023-06-05 22:05:18 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+function App() {
+  return (
+    <>
+      <div className="h-screen flex flex-col justify-center items-center">
+        <div>
+          <h1 className="font-bold">Hello World</h1>
+        </div>
+        <div>
+          <p>This is Vite React with Tailwind CSS</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
